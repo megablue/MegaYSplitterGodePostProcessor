db1b0db94a461ba27658f193d2423fed79172551
Evert Chin <evertchin@gmail.com>
2016-12-21 13:46:08 +0800

    First version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MegaYSplitterGodePostProcessor
 It is a FDM gcode post processor designed to improved multi materials printing with a Y splitter (such as Prometheus and Prusa i3 MK2 Multi materials printing kit) 
+
+It is highly experimental, currently only work with gcode rendered by Simplify3D. You need to insert to proper markers into the toolchange section of your S3D settings in order for the script to work.
+
+*Installation*
+npm install line-by-line
+
+*Execute*
+node toolchange.js yourgcodefile.gcode
---
+++ toolchange.js
@@ -0,0 +1,97 @@
+console.log("Mega YSplitter Gcode Post Processor v1.0");
+
+var args = process.argv.slice(2),
+    filename = args.join([separator = ' ']);
+
+if(filename.length == 0){
+	console.log("Please provide the path and filename for the gcode you want to processs. eg: node toolchange.js 3dbenchy.gcode");
+}
+
+var consts = require('constants'),
+    fs  = require("fs"), 
+    LineByLineReader = require('line-by-line'),
+    lr = new LineByLineReader(filename),
+    outputName = filename.split('.gcode')[0] + '_processed.gcode';
+
+//var startupTemplate = fs.readFileSync('startup-template.txt').toString();
+var toolchangeTemplate = fs.readFileSync('toolchange-template.txt').toString();
+var fd = fs.openSync('./' + outputName, 'w');
+var buffer = '';
+var toolchangeCount = 0;
+var foundToolChangeMarker = false;
+var newtoolNumber = -1;
+var oldtoolNumber = -1;
+//var startupMakerFound = false;
+//var matchStartup = /^;STARTUPBEGINS/;
+var matchToolChange =/^;TOOLCHANGE NEW(\d) OLD(\d)/;
+var matchToolChangeXY = /^G1 (X-?\d{0,}.\d{0,}) (Y-?\d{0,}.\d{0,})/i;
+
+lr.on('line', function (line) {
+	if(foundToolChangeMarker){
+		var xyResult = line.match(matchToolChangeXY);
+		if(xyResult != null){
+			var x = xyResult[1];
+			var y = xyResult[2];
+			var xy = x + ' ' + y;
+			//console.log('Next XY Coordinate: ' + xy);
+			buffer += renderToolChange(toolchangeTemplate, newtoolNumber, oldtoolNumber, xy);
+			fs.appendFileSync(fd, buffer + "\n");
+			buffer = '';
+			foundToolChangeMarker = false;
+		}
+		return;
+	}
+
+	var toolchangeResult = line.match(matchToolChange);
+	var toolSwitched = false;
+
+	if(toolchangeResult != null){
+		newtoolNumber = toolchangeResult[1];
+		oldtoolNumber = toolchangeResult[2];
+		
+		if(newtoolNumber != oldtoolNumber){
+			toolSwitched = true;
+			foundToolChangeMarker = true;
+		}
+
+		if(toolSwitched){
+			console.log('New tool: ' + newtoolNumber + ', Old tool: ' + oldtoolNumber);
+		} else {
+			console.log('Tool not switched');
+		}
+	} else {
+		buffer += line + "\n";
+	}
+});
+
+lr.on('error', function (err) {
+	// 'err' contains error object
+	console.log('ERROR!!!');
+	console.log(err);
+});
+
+lr.on('end', function () {
+	// All lines are read, file is closed now.
+	//write the remaining buffer into the output file
+	if(buffer.length > 0){
+		fs.appendFileSync(fd, buffer + "\n");
+	}
+
+	console.log('Gcode processed!');
+});
+
+// function renderStartup(template, z){
+// 	template = template.replace("\r\n", "\n");
+//     template = template.replace(/^\s*\n/gm, "\n");
+//     template = template.replace('{Z}', z);
+//     return template;
+// }
+
+function renderToolChange(template, newtool, oldtool, xy){
+    template = template.replace("\r\n", "\n");
+    template = template.replace(/^\s*\n/gm, "\n");
+    template = template.replace('{NEWTOOL}', newtool);
+    template = template.replace('{OLDTOOL}', oldtool);
+    template = template.replace('{XY}', xy);
+    return template;
+}
