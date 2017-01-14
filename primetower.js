4b4df5f2dd79b8f444e20fd71a3d1b8af8df2019
Evert Chin <evertchin@gmail.com>
2017-01-14 10:16:27 +0800

    Added ablity to generate bridges
    Added code to generate bridges and variable to control number of wipe

--- primetower.js
+++ primetower.js
@@ -5,12 +5,13 @@ var linewidth = 0.48,
     filamentpermm = (0.01662945).toFixed(5), //.1 layer height
     extrusionMultipler = 1,
     layerheight = 0.2,
-    firstlayerheight = 0.2,
+    firstlayerheight = 0.3,
     currentlayerheight = firstlayerheight,
     zOffset = 0.1,
     towerwidth = 3.84, //minimum tower width, if the total extrusion length cannot be fulfilled, program will increase the width to accommodate 
     towerlength = 90, //always fulfill the tower length
-    bridgelength = 40, //must be always less than the tower length
+    bridges = 3,
+    bridgehead = 2.4,
     speed = 3000,
     firslayerspeed = 1200,
     currentlayerspeed = 600,
@@ -24,7 +25,15 @@ var linewidth = 0.48,
     retraction = -9,
     retractionspeed = 1800,
     minimumPurgeLength = 30, //based on e3d v6 clone 1.75mm filament
-    prime = 9;
+    prime = 9,
+    wipe = 3;
+
+if(typeof wipe != 'number' || wipe < 0){
+	wipe = 1;
+}
+
+var totalBridgeHeads =  bridges + 1,
+	bridgeLength = (towerlength - (bridgehead * totalBridgeHeads)) / bridges;
 //end of vars
 if(centeriszero){
 	console.log("Center is zero");
@@ -43,11 +52,7 @@ if(centeriszero){
 	originY = bedYsize/2 - originY;
 }
 
-//offsets
-//originX += -85;
-//originY += 90;
-
-function primeTower(isFirstLayer, offsetX, offsetY, rotation, infillable_filament_length){
+function primeTower(isFirstLayer, offsetX, offsetY, rotation, infillableFilamentLength){
 
 	if(isFirstLayer){
 		currentlayerheight = firstlayerheight + zOffset;
@@ -59,7 +64,7 @@ function primeTower(isFirstLayer, offsetX, offsetY, rotation, infillable_filamen
 	fs.appendFileSync(fd, 'G0 F1800 Z' + (currentlayerheight + 1) + "\n");
 	drawUntil((originX + towerwidth).toFixed(3), originY, 0, 1800, "init point");
 
-	var filamentToBePurged = minimumPurgeLength - infillable_filament_length,
+	var filamentToBePurged = minimumPurgeLength - infillableFilamentLength,
 		x = 0, 
 		y = 0, 
 		z = currentlayerheight, 
@@ -68,18 +73,45 @@ function primeTower(isFirstLayer, offsetX, offsetY, rotation, infillable_filamen
 		savingMode = false,
 		flowRate = 1; 
 
-	if(infillable_filament_length > 0){
+	if(infillableFilamentLength > 0){
 		savingMode = true;
 	}
 
-	for(;x <= towerwidth || e < filamentToBePurged; x += linewidth){
-		if(x==0){
-			fs.appendFileSync(fd, 'G1 Z' + z + "\n"); // first layer z height
-			fs.appendFileSync(fd, "G1 F1500 E" + prime + "\n"); // prime a little
-			fs.appendFileSync(fd, "G92 E0\n"); // zeroing e length.
-		}
+	fs.appendFileSync(fd, 'G1 Z' + z + "\n"); // first layer z height
+	fs.appendFileSync(fd, "G1 F1500 E" + prime + "\n"); // prime a little
+	fs.appendFileSync(fd, "G92 E0\n"); // zeroing e length.
 
-		if(x==0){
+	//with saving mode we will generate bridgeheads 
+	if(savingMode){
+		console.log("savingMode detected");
+		for(var i = 0; i < totalBridgeHeads; i++){
+			for(var b = 0, c = 0; b <= bridgehead; b += linewidth, c++){
+				console.log("E length: " + eLength + ", Absolute E: " + e.toFixed(5));
+				var drawX = ((originX + towerwidth - linewidth).toFixed(3)),
+					drawY = ((c * linewidth + bridgeLength * i + i * bridgehead + originY).toFixed(3)),
+					drawE = (e).toFixed(5);
+					drawUntil(drawX, drawY, drawE, currentlayerspeed, "b=" + b + ", i=" + i + ", c=" + c);
+				e += getExtrusionLength(towerwidth);
+
+				console.log("E length: " + eLength + ", Absolute E: " + e.toFixed(5));
+				var drawX = ((originX).toFixed(3)),
+					drawY = ((c * linewidth + bridgeLength * i + i * bridgehead + originY).toFixed(3)),
+					drawE = (e).toFixed(5);
+					drawUntil(drawX, drawY, drawE, currentlayerspeed, "b=" + b + ", i=" + i + ", c=" + c);
+
+				if( !(i == totalBridgeHeads - 1 && b == bridgehead - linewidth)){
+					e += getExtrusionLength(towerwidth);
+				}
+
+				if(b == bridgehead){
+					console.log("bridgehead length: " + bridgehead);
+				}
+			}
+		}
+	}
+
+	for(var oddity = 0;x < towerwidth || e < filamentToBePurged; x += linewidth, oddity++){
+		if(x==0 && !savingMode){
 			// console.log("here11");
 			// fs.appendFileSync(fd, ";here11\n");
 		    console.log("E length: " + eLength + ", Absolute E: " + e.toFixed(5));
@@ -90,45 +122,85 @@ function primeTower(isFirstLayer, offsetX, offsetY, rotation, infillable_filamen
 			e += getExtrusionLength(towerwidth);	
 		}
 
-		if(x==linewidth){
+		if(x==linewidth && !savingMode){
 			// console.log("here");
 			// fs.appendFileSync(fd, ";Here\n");
 			e += getExtrusionLength(towerwidth);
 		    console.log("E length: " + eLength + ", Absolute E: " + e.toFixed(5));
-			var drawX = ((x + originX + towerwidth - linewidth - linewidth).toFixed(3)),
-				drawY = ((y + originY + towerlength).toFixed(3)),
+			var drawX = ((x + originX + towerwidth).toFixed(3)),
+				drawY = ((y + originY + towerlength - linewidth).toFixed(3)),
 				drawE = (e).toFixed(5);
-				drawUntil(drawX, drawY, drawE, currentlayerspeed, "draw second outline");
+				drawUntil(drawX - linewidth *2, drawY, drawE, currentlayerspeed, "draw second outline");
 				drawUntil(drawX-towerwidth, drawY, drawE, currentlayerspeed);
 				e += getExtrusionLength(towerlength);
 		}
 
-		var gotoX = ((x + originX).toFixed(3)),
-		    gotoY = ((y + originY).toFixed(3)),
-		    gotoE =  (e).toFixed(5);
+		if(!savingMode){
+			var gotoX = ((x + originX).toFixed(3)),
+			    gotoY = ((y + originY).toFixed(3)),
+			    gotoE =  (e).toFixed(5);
 
-		drawUntil(gotoX, gotoY, gotoE, currentlayerspeed);
+			drawUntil(gotoX, gotoY, gotoE, currentlayerspeed);
 
-		e += getExtrusionLength(towerlength);
-		console.log("E length: " + eLength + ", Absolute E: " + e.toFixed(5));
+			e += getExtrusionLength(towerlength);
+			console.log("E length: " + eLength + ", Absolute E: " + e.toFixed(5));
 
-		var drawX = ((x + originX).toFixed(3)),
-			drawY = ((y + originY + towerlength).toFixed(3)),
-			drawE = (e).toFixed(5);
-		drawUntil(drawX, drawY, drawE, currentlayerspeed);
+			var drawX = ((x + originX).toFixed(3)),
+				drawY = ((y + originY + towerlength - linewidth).toFixed(3)),
+				drawE = (e).toFixed(5);
+			drawUntil(drawX, drawY, drawE, currentlayerspeed);
+		} else{
+			if(oddity % 2 == 0){
+				console.log("Odd");
+				for(var i = totalBridgeHeads -1; i > 0; i--){
+					var drawX = ((x + originX).toFixed(3)),
+						drawY = ((bridgeLength * i + bridgehead * i + originY - linewidth/2).toFixed(3)),
+						drawE = (e).toFixed(5);
+						drawUntil(drawX, drawY, drawE, currentlayerspeed, "reversing start");
 
-	// console.log("X: " + (x).toFixed(5) * 1);
-	// console.log("towerwidth: " +  towerwidth);
-	// console.log("linewidth: " +  linewidth);
-	// console.log("x  == towerwidth - linewidth: " +  Math.floor(towerwidth - linewidth));
+						e += getExtrusionLength(bridgeLength * (i-1) + bridgehead * (i) + originY + linewidth/2);
 
-		if(e > filamentToBePurged && Math.floor(x) >= Math.floor(towerwidth - linewidth)){
+						drawX = ((x + originX).toFixed(3)),
+						drawY = ((bridgeLength * (i-1) + bridgehead * (i) + originY - linewidth/2).toFixed(3)),
+						drawE = (e).toFixed(5);
+
+						drawUntil(drawX, drawY, drawE, currentlayerspeed, "i =" + i);
+
+						console.log("Reverse E length: " + eLength + ", Absolute E: " + e.toFixed(5));
+				}
+			} else {
+				console.log("Even");
+				for(var i = 0; i < totalBridgeHeads - 1; i++){
+					var drawX = ((x + originX).toFixed(3)),
+						drawY = ((bridgeLength * i + bridgehead * (i+1) + originY - linewidth/2).toFixed(3)),
+						drawE = (e).toFixed(5);
+						drawUntil(drawX, drawY, drawE, currentlayerspeed);
+
+						e += getExtrusionLength(bridgeLength * (i+1) + bridgehead * (i+1) + originY + linewidth/2);
+
+						drawX = ((x + originX).toFixed(3)),
+						drawY = ((bridgeLength * (i+1) + bridgehead * (i+1) + originY + linewidth/2).toFixed(3)),
+						drawE = (e).toFixed(5);
+						drawUntil(drawX, drawY, drawE, currentlayerspeed, "i =" + i);
+						console.log("E length: " + eLength + ", Absolute E: " + e.toFixed(5));
+				}
+			}
+		}
+
+		if(e > filamentToBePurged && x >= (towerwidth - linewidth)){
 			console.log("end");
+			towerwidth = x;
 			drawX = drawX * 1;
 			drawE = drawE * 1;
-			drawUntil( drawX + linewidth - towerwidth, drawY,  drawE + retraction, currentlayerspeed);
-			drawUntil( drawX, drawY,  drawE + retraction, currentlayerspeed);
-			drawUntil( drawX + linewidth - towerwidth, drawY,  drawE + retraction, currentlayerspeed);
+
+			if(wipe > 1){
+				retraction = Math.floor(retraction / wipe);
+			}
+
+			for(var w=0; w < wipe; w++){
+				drawUntil( drawX + linewidth - towerwidth, drawY,  drawE + retraction, currentlayerspeed);
+				drawUntil( drawX, drawY,  drawE + retraction, currentlayerspeed);
+			}
 		}
 	}
 
@@ -152,7 +224,7 @@ function primeTower(isFirstLayer, offsetX, offsetY, rotation, infillable_filamen
 	}
 
 	function getExtrusionLength(distance){
-		eLength = ((filamentpermm * currentlayerheight / 0.1 * distance * extrusionMultipler).toFixed(5)) * 1;
+		eLength = ((filamentpermm * currentlayerheight / 0.1 * Math.abs(distance) * extrusionMultipler).toFixed(5)) * 1;
 		return eLength;
 	}
 
@@ -178,7 +250,7 @@ function primeTower(isFirstLayer, offsetX, offsetY, rotation, infillable_filamen
 
 var isFirstLayer = true;
 
-primeTower(isFirstLayer, -85, 0, 0, 15);
+primeTower(isFirstLayer, -85, 0, 0, 0);
 //primeTower(0,  85, 90, 0);
 
 
